75df935c033e1cf58b0389885d2d9a3d498e6860
Rishabh Agarwal <41911876+RishabhAgarwal123@users.noreply.github.com>
2023-02-05 16:53:00 +0530

    Front end setup
    Client side setup using antd, redux, redux-toolkit

---
+++ Payment System/client/src/App.js
@@ -0,0 +1,17 @@
+import './stylesheets/alignments.css';
+import './stylesheets/custom-components.css';
+import './stylesheets/form-elements.css';
+import './stylesheets/theme.css';
+import './stylesheets/typography.css';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Payment Gateway
+      </h1>
+    </div>
+  );
+}
+
+export default App;
