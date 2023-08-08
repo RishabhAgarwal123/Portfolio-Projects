e62247c4399b890fcd3cf061eb2c18800cbde6f3
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2023-08-08 23:10:24 +0530

    Setting loader

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'page-not-found', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  loadingProgress: number = 0;
+  isLoading: boolean = true;
+
+  constructor (private router: Router) {}
+  
+  ngOnInit(): void {
+    const interval = setInterval(() => {
+      this.loadingProgress += 1;
+      if (this.loadingProgress === 100) {
+        clearInterval(interval);
+        this.isLoading = false;
+        this.navigate(this.isLoading);
+      }
+    }, 10);
+  }
+
+  navigate(loading: boolean): any {
+    if (!loading) {
+      this.router.navigate(['./page-not-found']);
+    }
+  }
 }
