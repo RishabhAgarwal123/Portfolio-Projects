88b69f17a90bd060ad2cf26b5e7d6eaa03f303ce
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2023-08-14 16:27:43 +0530

    Orders and Pages completed
    Orders, Employees and Customers are completed

--- react-dashboard/src/components/Charts/SparkLine.jsx
+++ react-dashboard/src/components/Charts/SparkLine.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = () => {
+
+const SparkLine = ({color, currentColor, type, id, data, height, width}) => {
+  console.log(data)
   return (
-    <div>SparkLine</div>
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType='Numeric'
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName='x'
+      yName='yval'
+      type={type}
+      tooltipSettings={{
+        visible: true,
+        // eslint-disable-next-line no-template-curly-in-string
+        format: '${x} : data ${yval}',
+        trackLineSettings: {
+          visible: true
+        }
+      }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
   )
 }
 
