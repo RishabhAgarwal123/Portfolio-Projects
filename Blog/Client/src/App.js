ed668960fd4ad499c5e9a3c63b54f4918ed74223
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2023-09-30 23:24:01 +0530

    Login form

--- Blog/Client/src/App.js
+++ Blog/Client/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Post from './Post'
+import Login from './User/Login'
 
 const App = () => {
   return (
     <>
     <main>
-      <Navbar />
-      <Post />
+      {/* <Navbar />
+      <Post /> */}
+      <Login />
     </main>
     </>
   )
