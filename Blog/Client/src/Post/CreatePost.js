e92d1f9be9068505bac4d20a70aaf2c9d1ae970e
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2023-10-03 16:57:02 +0530

    Added react quill and capturing formdata
    Custom properties for react-quill

--- Blog/Client/src/Post/CreatePost.js
+++ Blog/Client/src/Post/CreatePost.js
@@ -2,12 +2,31 @@ import React, { useState } from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import './Post.css';
+import { customFormats, customModules } from '../utils/Custom';
 
 const CreatePost = () => {
+    const [postContent, setPostContent] = useState({
+        title: '',
+        summary: '',
+        content: ''
+    });
+    const [title, setTitle] = useState('');
+    const [summary, setSummary] = useState('');
+    const [content, setContent] = useState('');
     const [imagePreview, setImagePreview] = useState('');
 
     const makePost = () => {
 
+    }
+
+    const inputChange = (e) => {
+        if (e.target.name === 'image') handleImage(e);
+        else {
+            setPostContent({
+                ...postContent,
+                [e.target.name]: e.target.value
+            })
+        }
     }
 
     const handleImage = (event) => {
@@ -24,23 +43,37 @@ const CreatePost = () => {
     return (
         <>
             <form className='form-post' onSubmit={makePost}>
-                <input type='text' className='input' placeholder='Title' />
-                <input type='text' className='input' placeholder='Summary' />
+                <input
+                    type='text'
+                    className='input'
+                    placeholder='Title'
+                    value={title || ''}
+                    onChange={(e) => inputChange(e)} />
+                <input
+                    type='text'
+                    className='input'
+                    placeholder='Summary'
+                    value={summary || ''}
+                    onChange={(e) => inputChange(e)} />
                 <div className='upload'>
                     {imagePreview && <img src={imagePreview} alt='Avatar Preview' />}
                     <label className="custom-file-input">
                         <input
                             type="file"
-                            name="avatar"
+                            name="image"
                             value={''}
                             accept="image/*"
                             onChange={(e) => handleImage(e)}
-                            />
+                        />
                         <span className="custom-file-label">Choose a file</span>
                     </label>
                 </div>
-                <ReactQuill theme='snow' />
-                <button className='submit' style={{ marginTop: '20px'}}> Create Post</button>
+                <ReactQuill
+                    theme='snow'
+                    modules={customModules}
+                    formats={customFormats}
+                    value={content} />
+                <button className='submit' style={{ marginTop: '20px' }}> Create Post</button>
             </form>
         </>
     )
