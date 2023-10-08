f03e73e2e431b248eb518e6c28de6013b4e8a107
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2023-10-08 19:01:04 +0530

    Making loader center

--- Blog/Client/src/Loader.js
+++ Blog/Client/src/Loader.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Loader = () => {
     return (
-        <svg viewBox="25 25 50 50">
-            <circle r="20" cy="50" cx="50"></circle>
-        </svg>
+        <div className='loader'>
+            <svg viewBox="25 25 50 50">
+                <circle r="20" cy="50" cx="50"></circle>
+            </svg>
+        </div>
     )
 }
 
