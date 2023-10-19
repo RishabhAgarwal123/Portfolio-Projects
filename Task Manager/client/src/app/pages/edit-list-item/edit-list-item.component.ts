c0f8df93ec6e70f26b34f714cd266e34cc7a54e8
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2023-10-19 09:35:05 +0530

    Integrating update api

--- Task Manager/client/src/app/pages/edit-list-item/edit-list-item.component.ts
+++ Task Manager/client/src/app/pages/edit-list-item/edit-list-item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ListResponse } from 'src/app/models/list.model';
+import { TaskService } from 'src/app/task.service';
 
 @Component({
   selector: 'app-edit-list-item',
@@ -6,13 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-list-item.component.scss']
 })
 export class EditListItemComponent implements OnInit {
-
-  constructor() { }
+  listId: string = '';
+  constructor(private route: ActivatedRoute, private router: Router, private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => this.listId = params['id']
+    );
+
   }
 
   editListItem(value: string) {
-
+    this.taskService.updateList(this.listId, value).subscribe(
+      (res: ListResponse) => {
+        if (res.success) {
+          this.router.navigate(['/'])
+        }
+      },
+      (error) => console.log(error)
+    )
   }
 }
