7bb67ef39256105e2e3960bb584499ac51aee3e5
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2023-10-23 22:08:20 +0530

    Login and Register form

--- Task Manager/client/src/app/app-routing.module.ts
+++ Task Manager/client/src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { NewListItemComponent } from './pages/new-list-item/new-list-item.compon
 import { NewTaskComponent } from './pages/new-task/new-task.component';
 import { EditListItemComponent } from './pages/edit-list-item/edit-list-item.component';
 import { EditTaskComponent } from './pages/edit-task/edit-task.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   { path: '', component: TaskViewComponent },
   { path: 'new-list-item', component: NewListItemComponent },
   { path: 'edit-list-item/:id', component: EditListItemComponent },
   { path: 'new-task/:id', component: NewTaskComponent },
-  { path: 'edit-task/:id', component: EditTaskComponent }
+  { path: 'edit-task/:id', component: EditTaskComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ]
 
 @NgModule({
--- Task Manager/client/src/app/app.module.ts
+++ Task Manager/client/src/app/app.module.ts
@@ -14,6 +14,9 @@ import { NewTaskComponent } from './pages/new-task/new-task.component';
 // import { reducer } from './redux/reducer/app.reducer';
 import { EditListItemComponent } from './pages/edit-list-item/edit-list-item.component';
 import { EditTaskComponent } from './pages/edit-task/edit-task.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -23,7 +26,9 @@ import { EditTaskComponent } from './pages/edit-task/edit-task.component';
     NewListItemComponent,
     NewTaskComponent,
     EditListItemComponent,
-    EditTaskComponent
+    EditTaskComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -32,6 +37,8 @@ import { EditTaskComponent } from './pages/edit-task/edit-task.component';
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
     StoreRouterConnectingModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule
     // StoreModule.forRoot({ listId: reducer})
   ],
   providers: [],
