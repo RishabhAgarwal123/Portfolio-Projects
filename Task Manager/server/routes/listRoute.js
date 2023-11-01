a7c333620015745067f6d97cb0127a2b7690d4e3
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2023-11-01 21:20:35 +0530

    Protecting api routes

--- Task Manager/server/routes/listRoute.js
+++ Task Manager/server/routes/listRoute.js
@@ -7,10 +7,10 @@ const { createList,
 const { isAuthenticated } = require('../middlewares/sessionMiddleware');
 const router = express.Router();
 
-router.route('/lists').post(createList);
-router.route('/lists/:id').delete(deleteList);
+router.route('/lists').post(isAuthenticated, createList);
+router.route('/lists/:id').delete(isAuthenticated, deleteList);
 router.route('/lists').get(isAuthenticated, getAllList);
-router.route('/lists/:id').get(getSingleList);
-router.route('/lists/:id').patch(updateList);
+router.route('/lists/:id').get(isAuthenticated, getSingleList);
+router.route('/lists/:id').patch(isAuthenticated, updateList);
 
 module.exports = router;
--- Task Manager/server/routes/taskRoute.js
+++ Task Manager/server/routes/taskRoute.js
@@ -4,13 +4,14 @@ const { createTask,
     getAllTask,
     updateTask, 
     getSingleTask} = require('../controllers/taskController');
+const { isAuthenticated } = require('../middlewares/sessionMiddleware');
 const router = express.Router();
 
-router.route('/tasks/:id').post(createTask);
-router.route('/tasks/:id').delete(deleteTask);
-router.route('/tasks/:id').get(getAllTask);
-router.route('/tasks/:id').get(getAllTask);
-router.route('/tasks/single/:id').get(getSingleTask);
-router.route('/tasks/:id').patch(updateTask);
+router.route('/tasks/:id').post(isAuthenticated, createTask);
+router.route('/tasks/:id').delete(isAuthenticated, deleteTask);
+router.route('/tasks/:id').get(isAuthenticated, getAllTask);
+router.route('/tasks/:id').get(isAuthenticated, getAllTask);
+router.route('/tasks/single/:id').get(isAuthenticated, getSingleTask);
+router.route('/tasks/:id').patch(isAuthenticated, updateTask);
 
 module.exports = router;
