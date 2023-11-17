66f2629ad39e95c0c765ee44c9cc4dbaef0960b0
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2023-11-17 09:05:01 +0530

    Added github contributiions

--- portfolio/src/app/github-contribution/github-contribution.component.ts
+++ portfolio/src/app/github-contribution/github-contribution.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from './github.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-github-contribution',
@@ -7,16 +8,16 @@ import { GithubService } from './github.service';
   styleUrls: ['./github-contribution.component.scss']
 })
 export class GithubContributionComponent implements OnInit {
-  days: { date: string, contribution: boolean }[] = [];
-  contributions: Set<string> = new Set<string>();
+  days: { date: string, color: string, title: string }[] = [];
+  contributions: any = [];
 
-  constructor(private githubService: GithubService) {}
+  constructor(private githubService: GithubService) { }
 
   ngOnInit() {
     // Fetch GitHub Contributions data here
     this.githubService.getContributions('RishabhAgarwal123').subscribe((data: any) => {
-      console.log(data)
-      this.contributions = new Set(data?.contributions?.map((contribution: any) => contribution.date));
+      // this.contributions = data?.contributions?.map((contribution: any) => contribution.date));
+      this.contributions = data?.contributions;
 
       // After fetching data, generate contributions
       this.generateContributionsForYear(2023);
@@ -24,16 +25,37 @@ export class GithubContributionComponent implements OnInit {
   }
 
   generateContributionsForYear(year: number) {
-    const currentDate = new Date(year, 0, 1); // January 1st of the given year
-    for (let i = 0; i < 365; i++) {
-      const date = new Date(currentDate);
-      date.setDate(date.getDate() + i);
+    for (let i = 0; i < this.contributions?.length; i++) {
+      const contribution = this.contributions[i];
+      const color = this.findColor(contribution?.level);
+      const date = this.formatDate(contribution?.date);
+      let title = ''
 
-      // Check if there's a contribution for this date
-      const isContribution = this.contributions.has(date.toISOString());
+      if (contribution?.level === 0) title = `No contribution on ${date}`;
+      else if (contribution?.level === 1) title = `${contribution?.level} contribution on ${date}`;
+      else title = `${contribution?.level} contributions on ${date}`;
 
-      this.days.push({ date: date.toISOString(), contribution: isContribution });
+      this.days.push({ date: date, color: color, title: title });
     }
-    console.log(this.days[0].date)
+  }
+
+  findColor(level: number): string {
+    let color = '';
+    if (level === 1) color = '#57bfff';
+    else if (level === 2) color = '#2faafb';
+    else if (level === 3) color = '#0797f7';
+    else if (level === 4) color = '#0083e3';
+    else if (level > 4) color = '#006fcf';
+    return color;
+  }
+
+  formatDate(date: string) {
+    const [year, month, day] = date.split('-').map(Number);
+
+    // Create a Moment.js object
+    const dateObject = moment(`20${year}-${month}-${day}`);
+
+    // Format the date as "MMMM, Do"
+    return dateObject.format('MMMM, Do');
   }
 }
