deec5b1ef4a740c23c036133035c9b316dcb1d9d
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2023-11-17 22:20:01 +0530

    Completed UI for web version

--- portfolio/src/app/work/work.component.ts
+++ portfolio/src/app/work/work.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work.component.scss']
 })
 export class WorkComponent implements OnInit {
-
+  projects: any = [
+    { title: 'To Do', link: '#todo', description: 'To Do Application', technology: 'Angular', image: 'url' },
+    { title: 'Chat', link: '#chat', description: 'Chat Application', technology: 'React', image: 'url' },
+    { title: 'Gym Excercises', link: '#gym', description: '', technology: 'React', image: 'url' },
+    { title: 'Natours', link: '#natours', description: 'Travelling Application', technology: 'SASS', image: 'url' },
+    { title: 'Nexters', link: '#nexters', description: 'Travelling Application', technology: 'SASS', image: 'url' },
+  ];
   constructor() { }
 
   ngOnInit(): void {
