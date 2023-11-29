8d39af74e9ea25a7d2fa0912ab7b1eeb9b5d7da0
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2023-11-29 10:32:50 +0530

    Note Form

--- notes-taking/src/NoteForm.tsx
+++ notes-taking/src/NoteForm.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Col, Form, Row, Stack, Button } from 'react-bootstrap';
+import CreatableReactSelect from 'react-select';
 
 const NoteForm = () => {
   return (
-    <div>NoteForm</div>
+    <Form>
+      <Row>
+        <Col>
+          <Form.Group controlId='title'>
+            <Form.Label>Title</Form.Label>
+            <Form.Control required />
+          </Form.Group>
+        </Col>
+
+        <Col>
+          <Form.Group controlId='tags'>
+            <Form.Label>Tags</Form.Label>
+            <CreatableReactSelect isMulti />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Form.Group controlId='markdown'>
+        <Form.Label>Body</Form.Label>
+        <Form.Control required as='textarea' rows={15} />
+      </Form.Group>
+
+      <Stack direction='horizontal' gap={4} style={{marginTop: '20px'}} className='justify-content-end'>
+        <Button type='submit' variant='primary'> Save </Button>
+        <Button type='button' variant='outline-secondary'>Cancel</Button>
+      </Stack>
+    </Form>
   )
 }
 
