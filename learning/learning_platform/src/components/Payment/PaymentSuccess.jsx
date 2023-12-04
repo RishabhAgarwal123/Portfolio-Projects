f2f7e30f271277510e842d520eb3ccc63415a107
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2023-12-04 22:20:52 +0530

    Payment success page

--- learning/learning_platform/src/components/Payment/PaymentSuccess.jsx
+++ learning/learning_platform/src/components/Payment/PaymentSuccess.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Box, Button, Container, Heading, Text, VStack } from '@chakra-ui/react'
+import { Link } from 'react-router-dom';
+import { RiCheckboxCircleFill } from 'react-icons/ri';
 
 const PaymentSuccess = () => {
-  return (
-    <div>PaymentSuccess</div>
-  )
+  return <Container h={'90vh'} p={'16'}>
+    <Heading my={'8'} textAlign={'center'}>
+      You have Pro Pack
+    </Heading>
+    <VStack boxShadow={'lg'} pb={'16'} alignItems={'center'} borderRadius={'lg'}>
+      <Box w={'full'} bgColor={'blue.400'} p={'4'} css={{ borderRadius: '8px 8px 0 0' }}>
+        <Text color={'black'}>Payment Success</Text>
+      </Box>
+      <Box p={'4'}>
+        <VStack textAlign={'center'} px={'8'} mt={'4'} spacing={'8'}>
+          <Text>Congratulations! You've unlocked Pro membership, granting access to exclusive premium content.</Text>
+          <Heading size={'4xl'}>
+            <RiCheckboxCircleFill />
+          </Heading>
+        </VStack>
+      </Box>
+      <Link to='/profile'>
+        <Button variant={'ghost'}>
+          Explore profile
+        </Button>
+      </Link>
+      <Heading size={'xs'}>Reference: fdgdgddd, gjhsdjsd</Heading>
+    </VStack>
+  </Container>
 }
 
 export default PaymentSuccess
