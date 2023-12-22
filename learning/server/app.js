5c570ee854f235867e528b8645906e1a98857a96
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2023-12-22 10:05:27 +0530

    Error Handling

--- learning/server/app.js
+++ learning/server/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { config } from 'dotenv'
 import user from './routes/userRoute.js';
 import course from "./routes/courseRoute.js";
+import ErrorMiddleware from './middlewares/Error/errorMiddleware.js'
 
 config({
     path: './config/config.env'
@@ -11,5 +12,7 @@ const app = express();
 
 app.use('/api/v1', course);
 app.use('/api/v1', user);
+
+app.use(ErrorMiddleware);
 
 export default app;
---
+++ learning/server/middlewares/Error/errorMiddleware.js
@@ -0,0 +1,12 @@
+const ErrorMiddleware = (error, req, res, next) => {
+
+    error.statusCode = error.statusCode || 500;
+    error.message = error.message || 'Internal Server Error';
+
+    res.status(error.statusCode).json({
+        success: false,
+        message: error.message
+    })
+}
+
+export default ErrorMiddleware;
