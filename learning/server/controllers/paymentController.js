200d8a89a7b39905e9f13b08f4b55a97a96ebbc7
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2024-01-06 15:59:52 +0530

    Payment Verification

--- learning/server/controllers/paymentController.js
+++ learning/server/controllers/paymentController.js
@@ -2,6 +2,8 @@ import { catchAsyncError } from "../middlewares/CatchAsyncError/catchAsyncError.
 import { User } from "../models/userModel.js";
 import { RazorPayInstance } from "../server.js";
 import ErrorHandler from "../utils/errorHandler/errorHandler.js";
+import crypto from 'crypto';
+import { Payment } from "../models/paymentModel.js";
 
 export const buySubscription = catchAsyncError(async (req, res, next) => {
     const user = await User.findById(req.user._id);
@@ -26,5 +28,40 @@ export const buySubscription = catchAsyncError(async (req, res, next) => {
         success: true,
         message: 'User Subscription Started',
         subscriptionId: subscription.id
+    })
+});
+
+export const paymentVerification = catchAsyncError(async (req, res, next) => {
+    const { razorpay_payment_id, razorpay_signature, razorpay_subscription_id } = req.body;
+
+    const user = await User.findById(req.user._id);
+
+    const subscriptionId = user.subscription.id;
+
+    const generatedSignature = crypto.createHmac('sha256', process.env.RAZORPAY_API_SECRET
+    ).update(razorpay_payment_id + "|" + subscriptionId, "utf-8").digest('hex');
+
+    const isMatched = generatedSignature === razorpay_signature;
+
+    if (!isMatched) return res.redirect(`${process.env.FRONTEND_URL}/paymentfail`);
+
+    // Db comes here
+    await Payment.create({
+        razorpay_payment_id, 
+        razorpay_signature, 
+        razorpay_subscription_id
+    });
+
+    user.subscription.status='active';
+
+    await user.save();
+
+    res.redirect(`${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`);
+});
+
+export const getRazorPayKey = catchAsyncError (async (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        key: process.env.RAZORPAY_API_KEY
     })
 });
--- learning/server/routes/paymentRoute.js
+++ learning/server/routes/paymentRoute.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { buySubscription } from '../controllers/paymentController.js';
+import { buySubscription, getRazorPayKey, paymentVerification } from '../controllers/paymentController.js';
 import { isAuthenticated } from '../middlewares/Auth/auth.js';
 
 const router = express.Router();
 
 // Buy Subscription
 router.route('/subscribe').get(isAuthenticated, buySubscription);
+router.route('/razorpaykey').get(getRazorPayKey);
+
+// Payment Verfication
+router.route('/paymentverification').post(isAuthenticated, paymentVerification);
 
 export default router;
