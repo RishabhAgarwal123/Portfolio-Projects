dc3a6035d4acf88739d9469245f542481f0ad690
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2024-01-06 16:22:56 +0530

    Cancel Subscription

--- learning/server/routes/paymentRoute.js
+++ learning/server/routes/paymentRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { buySubscription, getRazorPayKey, paymentVerification } from '../controllers/paymentController.js';
+import { buySubscription, cancelSubscription, getRazorPayKey, paymentVerification } from '../controllers/paymentController.js';
 import { isAuthenticated } from '../middlewares/Auth/auth.js';
 
 const router = express.Router();
 
 // Buy Subscription
 router.route('/subscribe').get(isAuthenticated, buySubscription);
+router.route('/subscribe/cancel').delete(isAuthenticated, cancelSubscription);
 router.route('/razorpaykey').get(getRazorPayKey);
 
 // Payment Verfication
