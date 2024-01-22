8f459b48f1d55e041ad2b8ee279b761f0a9ed781
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2024-01-22 19:01:55 +0530

    Portfolio UI

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
   appLoaded: boolean = false;
+  isMobile: boolean = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.detectScreenSize();
+  }
+
+  ngOnInit() {
+    this.detectScreenSize();
+  }
+
+  private detectScreenSize() {
+    this.isMobile = window.innerWidth <= 600; // Adjust the threshold based on your requirements
+  }
 }
