1f69515cf353c0b98bd9f2d3dbdd57ac31fa0a24
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2024-02-02 08:47:02 +0530

    Setting up of files

--- Flight_Booking/Flight-Client/src/App.jsx
+++ Flight_Booking/Flight-Client/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Search from './components/Search/Search';
+import Support from './components/Support/Support';
+import Info from './components/Info/Info';
+import Lounge from './components/Lounge/Lounge';
+import Subscribe from './components/Subscribers/Subscribe';
+import Travellers from './components/Travellers/Travellers';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <Home />
+      <Search />
+      <Support />
+      <Info />
+      <Lounge />
+      <Travellers />
+      <Subscribe />
+      <Footer />
+    </div>
   )
 }
 
