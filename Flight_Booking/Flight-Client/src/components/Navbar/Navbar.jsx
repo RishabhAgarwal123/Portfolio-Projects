ca3bb3807c9a44e6204a50e7ce4aca9e03e6029e
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2024-02-03 14:56:44 +0530

    Setting up css

--- Flight_Booking/Flight-Client/src/components/Navbar/Navbar.jsx
+++ Flight_Booking/Flight-Client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { SiConsul } from "react-icons/si";
+import { BsPhoneVibrate } from "react-icons/bs";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar flex'>
+      <div className="navbarOne flex">
+        <div>
+          <SiConsul />
+        </div>
+        <div className="flex">
+          <li className="flex"><BsPhoneVibrate /> Support</li>
+          <li className="flex">Languages</li>
+        </div>
+      </div>
+    </div>
   )
 }
 
