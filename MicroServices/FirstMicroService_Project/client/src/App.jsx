265ad508f41e3a77bc3394b537508903847c270c
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2024-02-10 22:33:52 +0530

    Integrating posts api

--- MicroServices/FirstMicroService_Project/client/src/App.jsx
+++ MicroServices/FirstMicroService_Project/client/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import PostCreate from './components/PostCreate'
+import PostList from './components/PostList'
 
 const App = () => {
   return (
     <div className="container">
       <PostCreate />
+      <hr />
+      <h1>Post List</h1>
+      <PostList />
     </div>
   )
 }
--- MicroServices/FirstMicroService_Project/client/src/components/PostCreate.jsx
+++ MicroServices/FirstMicroService_Project/client/src/components/PostCreate.jsx
@@ -7,8 +7,7 @@ const PostCreate = () => {
     const onSubmit = async (e) => {
         e.preventDefault();
 
-        const res = await axios.post("http://localhost:4000/posts", { title: title }, { "Content-Type": "application/json" });
-         
+        await axios.post("http://localhost:4000/posts", { title: title }, { "Content-Type": "application/json" });
         setTitle('');
     }
 
@@ -16,10 +15,10 @@ const PostCreate = () => {
         <form onSubmit={onSubmit}>
             <div className="form-group" style={{ marginTop: '20px', marginBottom: '20px' }}>
                 <label htmlFor="">Post Title</label>
-                <input type="text" name="title" id="title" className="form-control" value={title} 
-                onChange={(e) => {
-                    setTitle(e.target.value)
-                }}
+                <input type="text" name="title" id="title" className="form-control" value={title}
+                    onChange={(e) => {
+                        setTitle(e.target.value)
+                    }}
                 />
             </div>
             <button className="btn btn-primary">Create Post</button>
