4ae3eb287dae6fdc89a4aa89133e6e804a459b67
Rishabh Agarwal <rishabhagarwal2005@gmail.com>
2024-02-24 08:43:16 +0530

    Removing e2e tests

--- learning/server/controllers/courseController.js
+++ learning/server/controllers/courseController.js
@@ -6,7 +6,20 @@ import cloudinary from 'cloudinary';
 import { Stats } from "../models/statsModel.js";
 
 export const getAllCourses = catchAsyncError(async (req, res, next) => {
-    const courses = await Course.find().select("-lectures");
+    const keyword = req.query.keyword || '';
+    const category = req.query.category || '';
+    const courses = await Course.find(
+        {
+            title: {
+                $regex: keyword,
+                $options: 'i'
+            },
+            category: {
+                $regex: category,
+                $options: 'i'
+            }
+        }
+    ).select("-lectures");
     res.status(200).json({
         success: true,
         courses
